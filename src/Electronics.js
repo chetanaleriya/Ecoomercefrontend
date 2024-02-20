63f6c538f11d909c0ca8fad6a7b68ebd1d13d351
chetanaleriya <chetanaleriya1037@gmail.com>
2024-02-20 12:18:48 +0530

    first commit

---
+++ src/Electronics.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react"
+import Table from 'react-bootstrap/Table';
+import {
+    MDBCard,
+    MDBCardImage,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBRow,
+    MDBCol,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+// import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
+function Electronics()
+{
+    const[apidata,setapidata]=useState([])
+    useEffect(()=>{
+        fetch("https://fakestoreapi.com/products/category/electronics").then((result)=>{
+             result.json().then((data)=>{
+                setapidata(data)
+
+            })
+        })
+    },[])
+
+    const navigate=useNavigate();
+    const handleClick=(pid)=>
+    {
+            const data={name:pid}
+            navigate("/details",{state:data})
+    }
+    return(
+        <div>
+
+<MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+{
+            apidata.map((item)=>
+      <MDBCol>
+        
+            <MDBCard className='h-100'>
+            <center><MDBCardImage  src={item.image} width="100px" height="150px" /></center>
+            <MDBCardBody>
+              <MDBCardTitle>{item.title}</MDBCardTitle>
+              <MDBCardTitle>{item.price*80} Rs</MDBCardTitle>
+              <MDBCardText>{item.description.substring(0,100)} </MDBCardText>
+              <center> <MDBBtn varient="primary" onClick={()=>handleClick(item.id)}>View Details {item.id}</MDBBtn></center>
+            </MDBCardBody>
+          </MDBCard> 
+           
+      </MDBCol>
+      
+      )
+        }
+     
+    </MDBRow>
+    </div>
+    )
+}
+export default Electronics
